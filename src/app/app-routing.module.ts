7c4c27f112b635abf51b7ad4cb159a99f20ad6dd
crazyrubix <muneebkt@gmail.com>
2020-04-20 23:14:33 +0530

    fixes not showing nav bar after login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,7 +26,7 @@ const routes: Routes = [
     component: ListcontactsComponent,
     canActivate: [AuthGuard]
   },
-  { path: '', redirectTo: 'login', pathMatch: 'full' }
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from './_services/auth.service';
 import { Router } from '@angular/router';
+import { BehaviorSubject, Observable, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,16 +10,23 @@ import { Router } from '@angular/router';
 })
 export class AppComponent implements OnInit {
   title = 'angular-skeleton';
+  isAuthenticatedObs = new Observable<boolean>((observer: Observer<boolean>) => {
+    setInterval(() => observer.next(this.authService.isAuthenticated())
+      , 1000);
+  });
+
+  
+  public isAuthenticated$: BehaviorSubject<boolean>
   userDetails = {
     userName: '',
     userToken: ''
   }
-  isAuthenticated = false;
   constructor(public authService: AuthService, private router: Router) {
-
+    //this.isAuthenticated$ = new BehaviorSubject(false)
   }
 
   ngOnInit(): void {
+    this.isAuthenticated$ = new BehaviorSubject(false)
     this.getUserDetails()
   }
 
@@ -30,13 +38,13 @@ export class AppComponent implements OnInit {
     this.userDetails.userName = this.authService.getUserName()
     this.userDetails.userToken = this.authService.getToken()
     if (this.userDetails.userToken && this.userDetails.userName) {
-      this.isAuthenticated = true;
+      this.isAuthenticated$.next(true);
     }
   }
   logout(e) {
     e.preventDefault()
+    this.isAuthenticated$.next(false);
     if (this.authService.signOut()) {
-      this.isAuthenticated = false;
       this.router.navigate(['/login'])
     }
   }
